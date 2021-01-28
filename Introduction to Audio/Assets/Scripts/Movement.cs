bd0282ec851d8c4901126250276ce39537b23e73
Fran Tena <tarod.net@gmail.com>
2021-01-29 00:16:58 +0100

    final movement and sounds

--- Introduction to Audio/Assets/Scripts/Movement.cs
+++ Introduction to Audio/Assets/Scripts/Movement.cs
@@ -6,6 +6,12 @@ public class Movement : MonoBehaviour
 {
     public float speed;
     public Vector3 movement;
+    public AudioClip[] audioClipArray;
+    public float timeBetweenShots = 0.9f;
+    public int playbacks = 3;
+
+    float timer = 0;
+    bool toggle = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +28,24 @@ public class Movement : MonoBehaviour
             transform.position = transform.position + new Vector3(movement.x * speed, 0, movement.z * speed);
         } else
         {
-            //Destroy(gameObject);
+            timer += Time.deltaTime;
+            if (timer > timeBetweenShots)
+            {
+                GetComponent<AudioSource>().PlayOneShot(RandomClip());
+                timer = 0;
+                playbacks--;
+            }
         }
 
+        if(playbacks == -1)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    AudioClip RandomClip()
+    {
+        return audioClipArray[Random.Range(0, audioClipArray.Length-1)];
     }
 }
