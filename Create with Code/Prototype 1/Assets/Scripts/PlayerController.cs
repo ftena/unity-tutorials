582355b7fc14500f6c1cd94a0b348cc9867c5cb0
Fran Tena <tarod.net@gmail.com>
2021-03-08 22:36:38 +0100

    add challenge 1 - plane programming

--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    // Required variables
+    public float speed = 15.0f;
+    public float turnSpeed = 35.0f;
+    public float horizontalInput = 0.0f;
+    public float forwardInput = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // We get the player input
+       horizontalInput = Input.GetAxis("Horizontal");
+       forwardInput = Input.GetAxis("Vertical");
+
        // Move the vehicle forward 
-       transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       // To slide: transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);       
+       // But we want to turn the vehicle (Vector3.up == y axis)
+       transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
