5e048d10f0f449c12b0806d75604d024d4a767a3
Fran Tena <tarod.net@gmail.com>
2021-03-17 23:04:49 +0100

    spawn game objects

---
+++ Create with Code/Prototype 2 Basic Gameplay/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[ ] animalPrefabs;
+    public float timeBetweenSpawn = 1.5f;
+    private float timer = 0f; // Time to control the spawn.
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if ( timer >= timeBetweenSpawn )
+        {
+            timer = 0f;
+            // Randomly generate animal index and spawn position
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);            
+        }
+    }
+}
