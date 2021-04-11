099a385fa44d542fbd6d92914c247d1d14c3e8c8
Fran Tena <tarod.net@gmail.com>
2021-04-11 19:11:21 +0200

    use animation: run, jump and fall

--- Create with Code/prototype-3-sounds-and-effects/Assets/Scripts/PlayerController.cs
+++ Create with Code/prototype-3-sounds-and-effects/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody playerRb; 
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true; // to prevent double jump
     public bool gameOver = false;
+    private Rigidbody playerRb; // reference to own Rigidbody
+    private Animator playerAnim;  // reference to own Animator
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         // Physics.gravity: the gravity applied to all rigid bodies in the Scene.
         Physics.gravity *= gravityModifier;
     }
@@ -22,10 +24,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Make player jump if spacebar pressed
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -36,8 +39,10 @@ public class PlayerController : MonoBehaviour
             isOnGround = true;
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            gameOver = true;
             Debug.Log("Game Over!");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
