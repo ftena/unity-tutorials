27016758584cb27a930e2c8e7664df43700eecb6
Fran Tena <tarod.net@gmail.com>
2021-06-27 20:40:57 +0200

    Bonus features for Prototype 3: ramdom objects, double jump

---
+++ Create with Code/prototype-3-sounds-and-effects-bonus/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true; // to prevent double jump
+    public bool gameOver = false;
+    public int numberOfJumpings = 2;
+    public ParticleSystem explosionParticle; // reference to a ParticleSystem
+    public ParticleSystem dirtParticle; // reference to a ParticleSystem
+    public AudioClip jumpSound; // reference to sound effect
+    public AudioClip crashSound; // reference to sound effect
+    private Rigidbody playerRb; // reference to own Rigidbody
+    private Animator playerAnim;  // reference to own Animator
+    private AudioSource playerAudio; // reference to own AudioSource
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        // Physics.gravity: the gravity applied to all rigid bodies in the Scene.
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Make player jump if spacebar pressed
+        if(Input.GetKeyDown(KeyCode.Space) && !gameOver && numberOfJumpings > 0)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            numberOfJumpings--;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);            
+
+            /* Tip: If you want the physics to feel a little more like a
+            double jump normally does in most games, you can reset
+            the player's velocity to 0 using "playerRb.velocity = Vector3.zero;"*/
+            playerRb.velocity = Vector3.zero;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            numberOfJumpings = 2;
+            isOnGround = true;
+            dirtParticle.Play();
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+        }
+    }
+}
---
+++ Create with Code/prototype-3-sounds-and-effects-bonus/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    private PlayerController playerControllerScript; // reference to the desired script
+    private Vector3 spawnPos = new Vector3(25, 0, 0); // spawn position for obstacles
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle ()
+    {
+        if (!playerControllerScript.gameOver)
+        {
+            int randomIndex = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomIndex], spawnPos, obstaclePrefabs[randomIndex].transform.rotation);
+        }
+    }
+}
