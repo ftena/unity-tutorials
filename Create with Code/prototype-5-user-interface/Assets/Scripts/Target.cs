c9642e51b3f39979c063fc64cca0d2fefe535149
Fran Tena <tarod.net@gmail.com>
2022-03-06 12:56:28 +0100

    add Game Manager

--- Create with Code/prototype-5-user-interface/Assets/Scripts/Target.cs
+++ Create with Code/prototype-5-user-interface/Assets/Scripts/Target.cs
@@ -8,25 +8,52 @@ public class Target : MonoBehaviour
     private float randomMinForce = 12.0f;
     private float randomMaxForce = 16.0f;
     private float randomTorque = 10.0f;
-    private float randomX = 4.0f;
-    private float randomY = 6.0f;
+    private float randomXRange = 4.0f;
+    private float ySpawnPos = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
 
-        targetRb.AddForce(Vector3.up * Random.Range(randomMinForce, randomMaxForce), ForceMode.Impulse);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
 
         // AddTorque applies the rotation
-        targetRb.AddTorque(Random.Range(-randomTorque, randomTorque), Random.Range(-randomTorque, randomTorque), Random.Range(-randomTorque, randomTorque), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
-        transform.position = new Vector3(Random.Range(-randomX, randomX), -randomY);
+        transform.position = RandomSpawnPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // We have a Sensor object in the scene to help us in this case.
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(randomMinForce, randomMaxForce);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-randomTorque, randomTorque);
+    }
+
+    Vector3 RandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(-randomXRange, randomXRange), -ySpawnPos);
+
     }
 }
