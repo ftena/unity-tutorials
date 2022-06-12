77c0202586fb899253d2581a1af62f8e5c5a672a
Fran Tena <tarod.net@gmail.com>
2022-06-12 19:36:00 +0200

    avoided to drive in mid-air

--- Create with Code/prototype-1-player-control/Assets/Scripts/PlayerController.cs
+++ Create with Code/prototype-1-player-control/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
     public float turnSpeed = 35.0f;
     public float horizontalInput = 0.0f;
     public float forwardInput = 0.0f;
+    [SerializeField] List<WheelCollider> allWheels;
+
     [SerializeField] private float horsePower = 0.0f;
     private Rigidbody playerRb;
     [SerializeField] GameObject centerOfMass;
@@ -17,6 +19,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed = 0.0f;
     [SerializeField] TextMeshProUGUI rpmText;
     [SerializeField] private float rpm = 0.0f;
+    [SerializeField] int wheelsOnGround;
 
     void Start()
     {
@@ -27,13 +30,16 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        // Update speedometer text
-        speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f); // 3.6 for hph
-        speedometerText.SetText("Speed: " + speed + " mph");
+        if (IsOnGround())
+        {
+            // Update speedometer text
+            speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f); // 3.6 for hph
+            speedometerText.SetText("Speed: " + speed + " mph");
 
-        // Update rpm text
-        rpm = Mathf.Round((speed % 30) * 40);
-        rpmText.SetText("RPM: " + rpm);
+            // Update rpm text
+            rpm = Mathf.Round((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+        }
     }
 
     /* We use FixedUpdate instead of Update because it's
@@ -42,14 +48,36 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         // We get the player input
-       horizontalInput = Input.GetAxis("Horizontal");
-       forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-       // Move the vehicle forward 
-       // before adding the speedometer: transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-       playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput); // Adds force relative to its coordinate system.
-       // To slide: transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);       
-       // But we want to turn the vehicle (Vector3.up == y axis)
-       transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        if (IsOnGround())
+        {
+           // Move the vehicle forward 
+           // before adding the speedometer: transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+           playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput); // Adds force relative to its coordinate system.
+           // To slide: transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);       
+           // But we want to turn the vehicle (Vector3.up == y axis)
+           transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
     }
 }
